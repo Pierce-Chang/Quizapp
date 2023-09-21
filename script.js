f40b58f57a726a5a5ca8ae9a1fd96873c26d0cb0
Pierce Chang <pierce@mailbox.org>
2023-09-21 21:47:43 +0200

    addet progressbar + few functions

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ let questions = [
 ];
 
 let currentQuestion = 0;
+let rightQuestions = 0;
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -66,19 +67,62 @@ function init() {
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
 
-    document.getElementById('questiontext').innerHTML = question['question'];
+    if (currentQuestion >= questions.length) {
+        //show end screen
+        document.getElementById('rightAnswers').innerHTML = rightQuestions;
+        document.getElementById('amountOfQuestions').innerHTML = currentQuestion;
+        document.getElementById('endScreen').style = '';
+        document.getElementById('questionBody').style = 'display: none';
+        document.getElementById('header-image').src = 'img/tropy.png';
+        document.getElementById('header-image').classList.add('tropy-endscreen');
+    } else { //show next question
+        let percent =  (currentQuestion + 1) / questions.length;
+        percent = Math.round(percent * 100);
+        document.getElementById('progress-bar').innerHTML = `${percent} %`;
+        document.getElementById('progress-bar').style = `width: ${percent}%`;
 
-    for (let i = 1; i <= 4; i++) {
-        const answer = question[`answer_${i}`];
-        document.getElementById(`answer_${i}`).innerHTML = answer;
+        let question = questions[currentQuestion];
+
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').textContent = question['question'];
+
+        for (let i = 1; i <= 4; i++) {
+            const answer = question[`answer_${i}`];
+            document.getElementById(`answer_${i}`).textContent = answer;
+        }
     }
 }
 
-function answer(selection) {
-    let question = questions[currentQuestion];
-    console.log('selected answer is', selection);
-    let selectedQuestionNumber = selection.slice(-1);
-    console.log('right answer is', question['right_answer']);
+function answer(selection) {  // selection is the parameter for the answer_i variable out of html code
+    let question = questions[currentQuestion];  //question get the count of the array questions[0,1,2,3...] in this case the actual "0" for the first question
+    let selectedQuestionNumber = selection.slice(-1); //give selectedQuestionNumber the value of last char from selection 'answer_(i)'
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if (selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightQuestions++;
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
